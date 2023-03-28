787ab4b7ec772ef00dc2760429744975d52542dd
Thomas Coulon <tcoulon19@ad.hume.vt.edu>
2023-03-28 10:54:21 -0400

    formatted with Black

--- CoulonSignalProject.py
+++ CoulonSignalProject.py
@@ -1,12 +1,22 @@
-#Coulon Signal Project
+# Coulon Signal Project
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-#generate_wave takes in a sampling frequency, time duration, and wave frequency, and target SNR in dB and outputs values for time (time_range) 
-#and sampled signal amplidude (sampled_wave) for a sine wave which can be plotted with matplotlib.
-def generate_wave(sampling_frequency, time_duration, wave_frequency, snr_dB):
 
+# generate_wave takes in a sampling frequency, time duration, and wave frequency, and target SNR in dB and outputs values for time (time_range)
+# and sampled signal amplidude (sampled_wave) for a sine wave which can be plotted with matplotlib.
+def generate_wave(sampling_frequency, time_duration, wave_frequency, snr_dB):
+    """
+    Generate a wave with noise. It is possible to sample the signal from an exponential distribution. In this case the noise is added to the wave and the time range is returned.
+
+    @param sampling_frequency - Sampling frequency in Hz.
+    @param time_duration - Time interval in seconds. Must be greater than 0.
+    @param wave_frequency - Frequency of the wave in Hz.
+    @param snr_dB - Signal to noise ratio in dB.
+
+    @return Tuple of two NumPy arrays. First element is time range second element is wave sampled from the signal
+    """
     time_step = 1 / sampling_frequency
     time_range = np.arange(0, time_duration, time_step)
     sampled_wave = np.sin(2 * np.pi * wave_frequency * time_range)
@@ -14,45 +24,56 @@ def generate_wave(sampling_frequency, time_duration, wave_frequency, snr_dB):
 
     return (time_range, sampled_wave)
 
-#add_noise takes in a clean signal and target SNR (dB) and outputs a signal with noise added
-def add_noise(signal, snr_dB):
 
+# add_noise takes in a clean signal and target SNR (dB) and outputs a signal with noise added
+def add_noise(signal, snr_dB):
     snr = 10 ** (snr_dB / 10)
     noise = np.random.randn(len(signal))
-    sum_signal_squared = np.sum(signal ** 2)
-    sum_noise_squared = np.sum(noise ** 2)
-    alpha = np.sqrt(sum_signal_squared / (snr * sum_noise_squared)) #alpha amplifies noise to meet SNR requirement
+    sum_signal_squared = np.sum(signal**2)
+    sum_noise_squared = np.sum(noise**2)
+    alpha = np.sqrt(
+        sum_signal_squared / (snr * sum_noise_squared)
+    )  # alpha amplifies noise to meet SNR requirement
     signal_with_noise = signal + alpha * noise
 
     return signal_with_noise
 
-#calculate_PSD takes in the sampled signal from generate_wave, the sampling frequency, and time duration, and outputs values of
-#PSD (PSD_values) and frequency (frequencies_to_plot) to be plotted in the PSD.
-def calculate_PSD(sampled_wave, sampling_frequency, time_duration):
 
-    fft_size = int(sampling_frequency * time_duration) #Use sample length for fft
+# calculate_PSD takes in the sampled signal from generate_wave, the sampling frequency, and time duration, and outputs values of
+# PSD (PSD_values) and frequency (frequencies_to_plot) to be plotted in the PSD.
+def calculate_PSD(sampled_wave, sampling_frequency, time_duration):
+    fft_size = int(sampling_frequency * time_duration)  # Use sample length for fft
     dft = np.fft.fft(sampled_wave, fft_size)
     dft = np.fft.fftshift(dft)
-    PSD_values = (abs(dft) ** 2) / fft_size #Square dft magnitude to get PSD and normalize by sample length
+    PSD_values = (
+        abs(dft) ** 2
+    ) / fft_size  # Square dft magnitude to get PSD and normalize by sample length
 
     frequency_resolution = sampling_frequency / fft_size
-    frequencies_to_plot = np.arange(-(fft_size * frequency_resolution) // 2, (fft_size * frequency_resolution) // 2, \
-        frequency_resolution)
+    frequencies_to_plot = np.arange(
+        -(fft_size * frequency_resolution) // 2,
+        (fft_size * frequency_resolution) // 2,
+        frequency_resolution,
+    )
 
     return (PSD_values, frequencies_to_plot)
 
 
-#Inputs
-sampling_frequency = 200000 #Hz
-time_duration = .01 #s
-wave_frequency = 1000 #Hz
-snr_dB_1 = 20 #dB
-snr_dB_2 = 5 #dB
+# Inputs
+sampling_frequency = 200000  # Hz
+time_duration = 0.01  # s
+wave_frequency = 1000  # Hz
+snr_dB_1 = 20  # dB
+snr_dB_2 = 5  # dB
 
 
-#Plot signal and PSD for snr_dB_1
-(time_range_1, sampled_wave_1) = generate_wave(sampling_frequency, time_duration, wave_frequency, snr_dB_1)
-(PSD_values_1, frequencies_to_plot_1) = calculate_PSD(sampled_wave_1, sampling_frequency, time_duration)
+# Plot signal and PSD for snr_dB_1
+(time_range_1, sampled_wave_1) = generate_wave(
+    sampling_frequency, time_duration, wave_frequency, snr_dB_1
+)
+(PSD_values_1, frequencies_to_plot_1) = calculate_PSD(
+    sampled_wave_1, sampling_frequency, time_duration
+)
 
 plt.figure(0)
 plt.plot(time_range_1, sampled_wave_1)
@@ -69,9 +90,13 @@ plt.title("Power Spectral Density, SNR = 20 dB")
 plt.savefig("PSD_snr_20dB.png")
 
 
-#Plot signal and PSD for snr_dB_2
-(time_range_2, sampled_wave_2) = generate_wave(sampling_frequency, time_duration, wave_frequency, snr_dB_2)
-(PSD_values_2, frequencies_to_plot_2) = calculate_PSD(sampled_wave_2, sampling_frequency, time_duration)
+# Plot signal and PSD for snr_dB_2
+(time_range_2, sampled_wave_2) = generate_wave(
+    sampling_frequency, time_duration, wave_frequency, snr_dB_2
+)
+(PSD_values_2, frequencies_to_plot_2) = calculate_PSD(
+    sampled_wave_2, sampling_frequency, time_duration
+)
 
 plt.figure(2)
 plt.plot(time_range_2, sampled_wave_2)
